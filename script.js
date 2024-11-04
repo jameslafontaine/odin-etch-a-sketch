c42e5de6c583c191be1cbd38d3be3511e3baf020
jameslafontaine <79965281+jameslafontaine@users.noreply.github.com>
2024-11-05 00:49:11 +1100

    Add a 16x16 grid of square divs to the webpage
    Set up some basic styles to form a grid of squares with black borders
    that fit nicely on the page. Also use JavaScript to dynamically populate
    the grid with squares with the intention of supporting custom
    grid sizes in the near future.

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 // Create 16x16 grid of divs
+
+let gridY = 16
+let gridX = 16
+
+let newSquare = null
+
+const grid = document.querySelector(".grid")
+
+for (let i = 0; i < gridY; i++) {
+    for (let j = 0; j < gridX; j++) {
+        newSquare = document.createElement("div")
+        newSquare.id = `square${j}-${i}`
+        newSquare.classList.add("square")
+        newSquare.style.width = `${Math.floor(100 / gridX)}%`
+        newSquare.style.width = `${Math.floor(100 / gridY)}%`
+        grid.appendChild(newSquare)
+    }
+}
