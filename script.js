1a0129da0f0af3a9bca5c8fb922aad316c017ae3
jameslafontaine <79965281+jameslafontaine@users.noreply.github.com>
2024-11-05 01:13:46 +1100

    Improve square scaling and added hover effect
    Fixed logic to scale squares correctly within a now fixed width and
    height of 960x800px. Added a hover effect to squares which changes
    their background colour when hovered over by adding the .hover class.

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 // Create 16x16 grid of divs
 
+const gridWidth = 960
+const gridHeight = 800
+
 let gridY = 16
 let gridX = 16
 
@@ -9,11 +12,20 @@ const grid = document.querySelector(".grid")
 
 for (let i = 0; i < gridY; i++) {
     for (let j = 0; j < gridX; j++) {
+        // create a square
         newSquare = document.createElement("div")
         newSquare.id = `square${j}-${i}`
         newSquare.classList.add("square")
-        newSquare.style.width = `${Math.floor(100 / gridX)}%`
-        newSquare.style.width = `${Math.floor(100 / gridY)}%`
+        newSquare.style.width = `${Math.floor(gridWidth / gridX)}px`
+        newSquare.style.height = `${Math.floor(gridHeight / gridY)}px`
+
+        newSquare.addEventListener("mouseenter", handleHover);
+
         grid.appendChild(newSquare)
     }
 }
+
+function handleHover() {
+    this.classList.add("hover")
+}
+
