fea94c9c30a507adb83a3de806a11403c7fc52ae
jameslafontaine <79965281+jameslafontaine@users.noreply.github.com>
2024-11-05 02:11:21 +1100

    Add ability to create a custom-sized new grid
    Added a button which prompts the user for a grid size that must be
    between 1 - 100 and creates a fresh grid with those dimensions.

--- script.js
+++ script.js
@@ -1,27 +1,46 @@
 // Create 16x16 grid of divs
 
-const gridWidth = 960
-const gridHeight = 800
+const GRID_WIDTH = 960
+const GRID_HEIGHT = 800
+const INITIAL_SIZE = 16
+const MAX_SIZE = 100
 
-let gridY = 16
-let gridX = 16
+let grid = document.querySelector(".grid")
 
-let newSquare = null
+populateGrid(INITIAL_SIZE)
 
-const grid = document.querySelector(".grid")
+newGridBtn = document.querySelector("#new-grid-btn")
 
-for (let i = 0; i < gridY; i++) {
-    for (let j = 0; j < gridX; j++) {
-        // create a square
-        newSquare = document.createElement("div")
-        newSquare.id = `square${j}-${i}`
-        newSquare.classList.add("square")
-        newSquare.style.width = `${Math.floor(gridWidth / gridX)}px`
-        newSquare.style.height = `${Math.floor(gridHeight / gridY)}px`
+newGridBtn.addEventListener("click", () => {
+    let newGridSize = prompt("Please enter the number of squares per side for the new grid: ")
 
-        newSquare.addEventListener("mouseenter", handleHover);
+    while (newGridSize > MAX_SIZE || newGridSize < 1) {
+        newGridSize = prompt("Please enter a number between 1 and 100")
+    }
 
-        grid.appendChild(newSquare)
+    grid.remove()
+    grid = document.createElement("div")
+    grid.classList.add("grid")
+    document.body.appendChild(grid)
+    populateGrid(newGridSize)
+})
+
+function populateGrid(gridSize) {
+    let newSquare = null
+    for (let i = 0; i < gridSize; i++) {
+        for (let j = 0; j < gridSize; j++) {
+            // create a square
+            newSquare = document.createElement("div")
+            newSquare.id = `square${j}-${i}`
+            newSquare.classList.add("square")
+            newSquare.style.minWidth = `${100 / gridSize}%`
+            newSquare.style.minHeight = `${100 / gridSize}%`
+
+            // add a hover effect
+            newSquare.addEventListener("mouseenter", handleHover);
+
+            grid.appendChild(newSquare)
+        }
     }
 }
 
